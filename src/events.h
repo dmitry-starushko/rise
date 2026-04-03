867dfb4501bf9be7ecc69e34d8dfa2ae0bd833eb
Dmitry Starushko <dmitry.starushko@gmail.com>
2026-04-03 03:33:11 +0300

    events module updated

--- src/events.h
+++ src/events.h
@@ -22,16 +22,25 @@
 #include "inline.h"
 #include <functional>
 #include <flat_map>
+#include <utility>
 
 enum class EVENT;
+
+auto consteval operator, (const EVENT ev, auto variant) noexcept {
+    return std::make_pair(ev, variant);
+}
 
 namespace event {
 
 template<EVENT> struct data;
-template<typename T, EVENT ... event> class handler;
 template<EVENT> class default_bus;
 template<EVENT> class fast_unsafe_bus;
 template<EVENT> class null_bus;
+template<typename T, EVENT ... event> class handler;
+template<EVENT, auto variant> class default_variant_bus;
+template<EVENT, auto variant> class fast_unsafe_variant_bus;
+template<EVENT, auto variant> class null_variant_bus;
+template<typename T, auto ... event_variant> class variant_handler;
 
 template<typename T, EVENT event> concept able_to_handle
 = requires(T &t, data<event> &d) {
@@ -39,6 +48,15 @@ template<typename T, EVENT event> concept able_to_handle
         t.handle(d)
     };
 };
+
+template<typename T, EVENT event, auto variant> concept able_to_handle_variant
+= requires(T &t, data<event> &d) {
+    {
+        t.template handle<variant>(d)
+    };
+};
+
+//--------------------------------------------------------------------------------
 
 template<EVENT event>
 class registry {
@@ -85,15 +103,6 @@ class default_bus: public registry<event> {
             }
         }
     }
-
-    INLINE
-    static void dispatch(data<event> &&info) noexcept {
-        dispatch(info);
-    }
-
-  private:
-
-    template<typename T, EVENT... e> friend class handler;
 };
 
 template<EVENT event>
@@ -111,15 +120,6 @@ class fast_unsafe_bus: public registry<event> {
             h(info);
         }
     }
-
-    INLINE
-    static void dispatch(data<event> &&info) noexcept {
-        dispatch(info);
-    }
-
-  private:
-
-    template<typename T, EVENT... e> friend class handler;
 };
 
 template<EVENT event>
@@ -134,14 +134,6 @@ class null_bus: public registry<event> {
     INLINE
     static void dispatch(data<event> &) noexcept {
     }
-
-    INLINE
-    static void dispatch(data<event> &&) noexcept {
-    }
-
-  private:
-
-    template<typename T, EVENT... e> friend class handler;
 };
 
 template<typename T, EVENT ... event>
@@ -173,6 +165,115 @@ class handler {
     }
 };
 
+//--------------------------------------------------------------------------------
+
+template<EVENT event, auto variant>
+class variant_registry {
+  public:
+
+    using _handler_t = std::function<void(data<event>&)>;
+
+    variant_registry() = delete;
+
+  private:
+
+    INLINE
+    static void connect(void *ptr, _handler_t &&f) noexcept {
+        _handlers.emplace(ptr, std::move(f));
+    }
+
+    INLINE
+    static void disconnect(void *ptr) noexcept {
+        _handlers.erase(ptr);
+    }
+
+  protected:
+
+    inline static std::flat_map<void *, _handler_t> _handlers{};
+    template<typename T, auto... e> friend class variant_handler;
+};
+
+template<EVENT event, auto variant>
+class default_variant_bus: public variant_registry<event, variant> {
+  public:
+
+    using inherited = variant_registry<event, variant>;
+    using _handler_t = variant_registry<event, variant>::_handler_t;
+
+    default_variant_bus() = delete;
+
+    INLINE
+    static void dispatch(data<event> &info) noexcept { // TODO rename ev_data
+        std::vector<void *> hs(inherited::_handlers.keys());
+
+        for (auto h : hs) {
+            if (auto itr = inherited::_handlers.find(h); itr != inherited::_handlers.end()) {
+                itr->second(info);
+            }
+        }
+    }
+};
+
+template<EVENT event, auto variant>
+class fast_unsafe_variant_bus: public variant_registry<event, variant> {
+  public:
+
+    using inherited = variant_registry<event, variant>;
+    using _handler_t = variant_registry<event, variant>::_handler_t;
+
+    fast_unsafe_variant_bus() = delete;
+
+    INLINE
+    static void dispatch(data<event> &info) noexcept {
+        for (const auto &h : inherited::_handlers.values()) {
+            h(info);
+        }
+    }
+};
+
+template<EVENT event, auto variant>
+class null_variant_bus: public variant_registry<event, variant> {
+  public:
+
+    using inherited = variant_registry<event, variant>;
+    using _handler_t = variant_registry<event, variant>::_handler_t;
+
+    null_variant_bus() = delete;
+
+    INLINE
+    static void dispatch(data<event> &) noexcept {
+    }
+};
+
+template<typename T, auto ... event_variant>
+class variant_handler {
+    template<EVENT e, auto v>
+    INLINE
+    void bus_connect() noexcept {
+        T *self = static_cast<T *>(this);
+        default_variant_bus<e, v>::connect(self, [self] INLINE (data<e> &ed) noexcept {
+            self->template handle<v>(ed);
+        });
+    }
+
+    template<EVENT e, auto v>
+    INLINE
+    void bus_disconnect() noexcept {
+        default_variant_bus<e, v>::disconnect(static_cast<T *>(this));
+    }
+
+  protected:
+
+    INLINE variant_handler() noexcept {
+        static_assert((able_to_handle_variant<T, event_variant.first, event_variant.second> && ...));
+        (bus_connect<event_variant.first, event_variant.second>(), ...);
+    }
+
+    INLINE ~variant_handler() noexcept {
+        (bus_disconnect<event_variant.first, event_variant.second>(), ...);
+    }
+};
+
 template<EVENT event>
 INLINE
 inline void dispatch(data<event> &e) noexcept {
@@ -185,8 +286,22 @@ inline void dispatch(data<event> &&e) noexcept {
     dispatch(e);
 }
 
+template<auto variant, EVENT event>
+INLINE
+inline void dispatch(data<event> &e) noexcept {
+    default_variant_bus<event, variant>::dispatch(e);
+}
+
+template<auto variant, EVENT event>
+INLINE
+inline void dispatch(data<event> &&e) noexcept {
+    dispatch<variant>(e);
+}
+
 #define USE_NULL_BUS(E) template<> class event::default_bus<EVENT::E>: public event::null_bus<EVENT::E> { };
 #define USE_FAST_UNSAFE_BUS(E) template<> class event::default_bus<EVENT::E>: public event::fast_unsafe_bus<EVENT::E> { };
+#define USE_NULL_VARIANT_BUS(E,V) template<> class event::default_variant_bus<EVENT::E, V>: public event::null_variant_bus<EVENT::E, V> { };
+#define USE_FAST_UNSAFE_VARIANT_BUS(E,V) template<> class event::default_variant_bus<EVENT::E, V>: public event::fast_unsafe_variant_bus<EVENT::E, V> { };
 
 }
 
